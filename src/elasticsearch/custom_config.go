14aa33b020e3e12e993c449be93e3d510584cadf
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2020-08-05 00:51:17 +0100

    fixup! [WIP] Add an Elasticsearch endpoint

--- src/elasticsearch/custom_config.go
+++ src/elasticsearch/custom_config.go
@@ -1,16 +1,9 @@
-package config
+package main
 
 import (
-	"net/http"
 	"os"
-	"strconv"
-	"strings"
-	"time"
 
 	generic_config "github.com/alphagov/paas-prometheus-endpoints/pkg/config"
-
-	"code.cloudfoundry.org/lager"
-	cfclient "github.com/cloudfoundry-community/go-cfclient"
 )
 
 type Config struct {
@@ -23,7 +16,7 @@ type Config struct {
 
 func NewCustomConfigFromEnv(defaultServiceName string) Config {
 	return Config{
-		generic_config.NewConfigFromEnv(defaultServiceName),
+		Config: generic_config.NewConfigFromEnv(defaultServiceName),
 
 		AivenProjectName:        os.Getenv("AIVEN_PROJECT_NAME"),
 		AivenPrometheusUsername: os.Getenv("AIVEN_PROMETHEUS_USERNAME"),
--- src/elasticsearch/metric_fetcher.go
+++ src/elasticsearch/metric_fetcher.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
+	// "net/http"
 	"sync"
-	"time"
 
-	"github.com/alphagov/paas-observability-release/src/aiven-service-discovery/pkg/writer"
+	aiven_service_discovery_writer "github.com/alphagov/paas-observability-release/src/aiven-service-discovery/pkg/writer"
 	"github.com/alphagov/paas-prometheus-endpoints/pkg/authenticator"
 	"github.com/alphagov/paas-prometheus-endpoints/pkg/metric_endpoint"
 
 	"code.cloudfoundry.org/lager"
-	"github.com/aws/aws-sdk-go/service/cloudwatch"
-	"github.com/aws/aws-sdk-go/service/elasticache"
 	cfclient "github.com/cloudfoundry-community/go-cfclient"
 	"github.com/gin-gonic/gin"
 )
@@ -23,7 +21,7 @@ type ElasticsearchMetricFetcher struct {
 
 	logger lager.Logger
 
-	aivenServicePrometheusTargets map[string]writer.PrometheusTargetConfig
+	aivenServicePrometheusTargets map[string]aiven_service_discovery_writer.PrometheusTargetConfig
 	mu                            sync.RWMutex
 }
 
@@ -45,7 +43,7 @@ func NewElasticsearchMetricFetcher(
 
 type ServiceInstancePrometheusTargetConfig struct {
 	ServiceInstance        cfclient.ServiceInstance
-	PrometheusTargetConfig writer.PrometheusTargetConfig
+	PrometheusTargetConfig aiven_service_discovery_writer.PrometheusTargetConfig
 }
 
 func (f *ElasticsearchMetricFetcher) FetchMetrics(
@@ -55,11 +53,11 @@ func (f *ElasticsearchMetricFetcher) FetchMetrics(
 	servicePlans []cfclient.ServicePlan,
 	service cfclient.Service,
 ) ([]metric_endpoint.Metric, error) {
-	logger := f.logger.Session("fetch-metrics", lager.Data{
-		"username": user.Username(),
-	})
+	// logger := f.logger.Session("fetch-metrics", lager.Data{
+	// 	"username": user.Username(),
+	// })
 
-	prometheusTargets := f.findPrometheusEndpoints(serviceInstances)
+	// prometheusTargets := f.findPrometheusEndpoints(serviceInstances, logger)
 
 	// FIXME: Scrape the `/metrics` endpoint for all those prometheus targets,
 	// using the needed basic auth.
@@ -98,13 +96,61 @@ func (f *ElasticsearchMetricFetcher) findPrometheusEndpoints(
 	return prometheusTargets
 }
 
-func (f *ElasticsearchMetricFetcher) WritePrometheusTargetConfigs(targets []writer.PrometheusTargetConfig) {
-	aivenServicePrometheusTargets := map[string]writer.PrometheusTargetConfig{}
+func (f *ElasticsearchMetricFetcher) WritePrometheusTargetConfigs(
+	targets []aiven_service_discovery_writer.PrometheusTargetConfig,
+) {
+	aivenServicePrometheusTargets := map[string]aiven_service_discovery_writer.PrometheusTargetConfig{}
 	for _, target := range targets {
-		aivenServicePrometheusTargets[target.Labels.ServiceName] = aivenServicePrometheusTargets
+		aivenServicePrometheusTargets[target.Labels.ServiceName] = target
 	}
 
 	f.mu.Lock()
 	defer f.mu.Unlock()
 	f.aivenServicePrometheusTargets = aivenServicePrometheusTargets
 }
+
+// // FIXME: Should this acceptheader be simplified in case endpoint behaviour will change?
+// const acceptHeader = `application/openmetrics-text; version=0.0.1,text/plain;version=0.0.4;q=0.5,*/*;q=0.1`
+// const userAgent = `paas-prometheus-endpoints-elasticsearch`
+
+// func scrapePrometheusTargets(targets []ServiceInstancePrometheusTargetConfig) ([]metric_endpoint.Metric, error) {
+// 	for _, target := range targets {
+// 		for _, nodeIP := range target.PrometheusTargetConfig.Targets {
+// 			// FIXME: Fetch the `/metrics` endpoint over HTTPS with the basic auth creds
+// 			// the parent object.
+// 			// FIXME: Surely I can use a library for this!
+
+// 			// Adapted from https://github.com/prometheus/prometheus/blob/f482c7bdd734ba012f6cedd68ddc24b1416efb35/scrape/scrape.go#L601
+// 			// FIXME: Package this as a separate library under the same license,
+// 			// see https://twitter.com/markspolakovs/status/1288953990533853189
+// 			url := fmt.Sprintf("https://%s/metrics", nodeIP.String())
+// 			req, err := http.NewRequest("GET", url, nil)
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			req.Header.Add("Accept", acceptHeader)
+// 			// FIXME: Support gzip since the originating code does?
+// 			//req.Header.Add("Accept-Encoding", "gzip")
+// 			req.Header.Set("User-Agent", userAgent)
+
+// 			// FIXME: Perform request
+
+// 			resp, err := s.client.Do(s.req.WithContext(ctx))
+// 			defer resp.Body.Close()
+
+// 			if resp.StatusCode != http.StatusOK {
+// 				return nil, errors.Errorf("server returned HTTP status %s", resp.Status)
+// 			}
+
+// 			textParser := expfmt.TextParser{}
+// 			metricFamilies, err := textParser.TextToMetricFamilies(resp.Body)
+// 			if err != nil {
+// 				// FIXME: Do something
+// 				// Perhaps return a slice or errors, rather than eager failing all of these?
+// 				return nil, err
+// 			}
+
+// 			return nil, nil
+// 		}
+// 	}
+// }
